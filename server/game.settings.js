49b4bb0c3c93febb970fd0501d4a415f7ebbc50a
Stefano Balietti <futur.dorko@gmail.com>
2014-11-10 18:03:53 +0100

    channels and settings

---
+++ server/game.settings.js
@@ -0,0 +1,18 @@
+/**
+ * # Game settings: Ultimatum Game
+ * Copyright(c) 2014 Stefano Balietti
+ * MIT Licensed
+ *
+ * http://www.nodegame.org
+ * ---
+ */
+
+module.exports = {
+
+    // Files
+    gamePaths: {
+        logic:  "game.logic.js",
+        player: "game.client.js"
+    }
+
+};
